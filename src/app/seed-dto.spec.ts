288edbf4d0a2e6696df104a73db2fa5a63d71c4e
Sweetha03 <121707446+Sweetha03@users.noreply.github.com>
2023-02-24 19:54:08 +0530

    Delete src directory

--- src/app/seed-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SeedDto } from './seed-dto';
-
-describe('SeedDto', () => {
-  it('should create an instance', () => {
-    expect(new SeedDto()).toBeTruthy();
-  });
-});
--- src/app/seed-pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SeedPipe } from './description-pipe';
-
-describe('SeedPipe', () => {
-  it('should create an instance', () => {
-    expect(new SeedPipe()).toBeTruthy();
-  });
-});
